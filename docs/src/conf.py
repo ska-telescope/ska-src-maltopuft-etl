8b18eadb51168261af52a288a97ff529114696d9
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-19 10:55:19 +0100

    TEAL-658: Configure docs and readthedocs integration

---
+++ docs/src/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "ska-src-maltopuft-etl"
+copyright = "2024, Elliot Goodwin"
+author = "Elliot Goodwin"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    # Auto document packages
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "recommonmark",
+]
+
+autoapi_dirs = ["../../src"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = [".rst", ".md"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "ska_ser_sphinx_theme"
+html_static_path = []
---
+++ docs/src/index.rst
@@ -0,0 +1,37 @@
+.. ska-src-maltopuft-etl documentation master file.
+
+.. HOME SECTION ==================================================
+.. Hidden toctree to manage the sidebar navigation.
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Home
+  :hidden:
+
+
+MALTOPUFT ETL documentation
+===========================
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Contents:
+
+.. README ========================================================
+..
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Readme
+
+  README
+
+.. PAGES ========================================================
+..
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Developer documentation
+  :hidden:
+
+  pages/maltopuft-etl.rst
+  autoapi/index.rst
