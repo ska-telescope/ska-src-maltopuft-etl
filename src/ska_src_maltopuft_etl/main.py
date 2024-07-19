8129ec92f0202407d49d4ae8d93d23957bd13da0
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-19 12:42:03 +0100

    TEAL-658: Configure logging

--- src/ska_src_maltopuft_etl/main.py
+++ src/ska_src_maltopuft_etl/main.py
@@ -1,2 +1,10 @@
+"""ska-src-maltopuft-etl entrypoint."""
+
+import logging
+
+from ska_ser_logging import configure_logging
+
 if __name__ == "__main__":
-    print("Hello world!")
+    configure_logging(logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info("Hello world!")
