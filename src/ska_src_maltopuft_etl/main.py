0c334a4542b29bc7cbb7776cedd1d4b0cb7d83be
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-22 15:22:58 +0100

    TEAL-663: Extract candidate data from spccl

--- src/ska_src_maltopuft_etl/main.py
+++ src/ska_src_maltopuft_etl/main.py
@@ -1,13 +1,22 @@
 """ska-src-maltopuft-etl entrypoint."""
 
 import logging
+from pathlib import Path, PosixPath
 
+import polars as pl
 from ska_ser_logging import configure_logging
 
+from ska_src_maltopuft_etl.core.config import config
 from ska_src_maltopuft_etl.meertrap.meertrap import extract
 
 if __name__ == "__main__":
     configure_logging(logging.INFO)
     logger = logging.getLogger(__name__)
 
-    extract()
+    output_path: PosixPath = config.get("output_path", Path())
+    cand_df: pl.DataFrame = extract()
+
+    output_parquet = output_path / "candidates.parquet"
+    logger.info(f"Writing parsed data to {output_path}")
+    cand_df.write_parquet(output_parquet, compression="gzip")
+    logger.info(f"Parsed data written to {output_path} successfully")
