8fb4272fe3500d24ab7bb7e394f66102025dba47
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-22 14:19:17 +0100

    TEAL-659: Extract Meerkat observation metadata from run summary

--- src/ska_src_maltopuft_etl/main.py
+++ src/ska_src_maltopuft_etl/main.py
@@ -1,11 +1,13 @@
 """ska-src-maltopuft-etl entrypoint."""
 
 import logging
-from ska_src_maltopuft_etl.core.config import config
 
 from ska_ser_logging import configure_logging
+
+from ska_src_maltopuft_etl.meertrap.meertrap import extract
 
 if __name__ == "__main__":
     configure_logging(logging.INFO)
     logger = logging.getLogger(__name__)
-    logger.info("Hello world!")
+
+    extract()
