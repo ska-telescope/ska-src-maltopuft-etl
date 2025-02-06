806a266d70f3adfcf18592516b8e4bf74fee5d46
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2025-02-06 18:01:04 +0000

    DAAC-456: Temporary log to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-python src/ska_src_maltopuft_etl/meertrap/main.py
-python src/ska_src_maltopuft_etl/atnf/main.py
+# TODO: Implement file based logging
+python src/ska_src_maltopuft_etl/meertrap/main.py 2>&1 | tee logs/meertrap_$(date +%d.%m.%yT%H:%M:%S).log
+python src/ska_src_maltopuft_etl/atnf/main.py 2>&1 | tee logs/atnf_$(date +%d.%m.%yT%H:%M:%S).log
