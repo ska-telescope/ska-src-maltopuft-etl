047a2520e818dd1dd27bbfa512a300d3f7e783f7
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2025-02-19 10:07:37 +0000

    DAAC-464: Asset jobs
    Asset jobs for "meertrap_pipeline" and "atnf_pipeline" are configured. This makes navigation on the UI more intuitive and means that jobs can be triggered with the dagster cmd line tool. For example:
    
    dagster job backfill -l ska-src-maltopuft-etl -j meertrap_pipeline --from <start-date> --to <end-date>

--- src/dagster/meertrap/definitions.py
+++ src/dagster/meertrap/definitions.py
@@ -1,11 +1,13 @@
 """Dagster asset configuration."""
 
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, define_asset_job, load_assets_from_modules
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
+all_assets_job = define_asset_job(name="meertrap_pipeline")
 
 defs = Definitions(
     assets=all_assets,
+    jobs=[all_assets_job],
 )
